27bcc1f3824876407e1874ab105df700035e4c9d
Lucas <80419418+lsena-web@users.noreply.github.com>
2021-11-28 21:18:07 -0300

    new tables

--- ricefit.sql
+++ ricefit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22-Nov-2021 às 23:30
+-- Tempo de geração: 29-Nov-2021 às 01:17
 -- Versão do servidor: 10.4.21-MariaDB
 -- versão do PHP: 8.0.10
 
@@ -41,7 +41,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `nome`, `email`, `celular`, `senha`, `anexo`) VALUES
-(1, 'Lucas sena', 'admin@gmail.com', '88981754649', '$2y$10$jO0rhg2pJRyKdK.pRichs.ie6BiViKVNcsV7EN8VjcAzZbVyvAA6a', '618dac62e387a.jpg');
+(1, 'Lucas sena', 'admin@gmail.com', '88981754649', '$2y$10$8aH6j6H6fKBi6zw3JRCWp.MmThCG0RG1sc/JnyVIx8Ul6vDyTjd8u', '618dac62e387a.jpg');
 
 -- --------------------------------------------------------
 
@@ -59,20 +59,16 @@ CREATE TABLE `alunos` (
   `condicao` enum('s','n') NOT NULL,
   `anexo` varchar(60) NOT NULL,
   `descricao` text NOT NULL,
-  `turma` varchar(30) NOT NULL
+  `turma` varchar(30) NOT NULL,
+  `link` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `alunos`
 --
 
-INSERT INTO `alunos` (`id`, `nome`, `email`, `celular`, `senha`, `sexo`, `condicao`, `anexo`, `descricao`, `turma`) VALUES
-(5, 'lucas sena', 'lucas@gmail.com', '88992450331', '$2y$10$d2USufP.iXwTpQST/kQJre9z/.mhBuGpZXBWBsDD9Wz9YkhqDlvP2', 'm', 's', '615dfb4e0543e.jpg', '                                                                                                                                                                                                                                                                                                                                                treinamento de hipertrofia                                                                                                                                                                                                                                                                                                                     ', 'bodybuilder'),
-(6, 'Damião lucas', 'l@gmail.com', '88981754649', '$2y$10$FDSM0SYtMoHieaU6o4UAOOn4O3mdRyt4951GWK1KBaAHHm7vyL4g6', 'm', 's', '615dfb35b7d58.png', '                                                amador                                            ', 'bodybuilder'),
-(8, 'lucas dev', 'dev@gmail.com', '99999999999', '$2y$10$6vlngoJ98F4n9UuS34HgyepDSMqfSkoRI1Xg8kZNCnIHp.SRy.Sxa', 'm', 'n', '618c054cce2e2.jpg', '<p>descrição teste</p>', 'bodybuilder'),
-(9, 'Cosmo Samuel Alencar de Sena', 'gerente@gerente.com', '88888888888', '$2y$10$g8pzQZ7Dda5F77jQq.pZ/u9s0VO2ijNbStr1DAgzOGwN4U2.WVZte', 'm', 's', '618dacf4c9608.jpg', 'hipertrofia', 'bodybuilder'),
-(10, 'zefinha', 'zefinha@gmail.com', '88977776666', '$2y$10$2oBBC40L0q7PTiQFh8JeNOUf1aWDYyY7S7LvUFbPKR3zLqAXd1.ym', 'f', 's', '61998b3aaa585.jpg', '<p>testando cadastro</p>', 'crossFit'),
-(11, 'josefa', 'josefa@gmail.com', '77655554444', '$2y$10$FPjfLp2qPuOM.kyhmKXlreN4nsiMNWLdce/bWpbACcGOM.4re4Tky', 'f', 's', '61998cb2add06.jpg', '<p>testando cadastro de josefa</p>', 'bodybuilder');
+INSERT INTO `alunos` (`id`, `nome`, `email`, `celular`, `senha`, `sexo`, `condicao`, `anexo`, `descricao`, `turma`, `link`) VALUES
+(1, 'lucas sena', 'lsena.web@gmail.com', '88999642994', '$2y$10$YlPzWpgM5D0JFHal1juYp.eKWHhJSuO.eHubWNPQd.6cZjkXa1IQS', 'm', 's', '61a3899f5438c.jpg', '<span style=\"color: rgb(0, 0, 0); font-family: \"Open Sans\", Arial, sans-serif; font-size: 14px; text-align: justify;\">é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</span>', 'bodybuilder', '$2y$10$zX3Rq6lgWRqLs5VN91SQaOE32Pge5SCgkcSFDj2wr.yVDpGTJSSzm');
 
 -- --------------------------------------------------------
 
@@ -101,15 +97,40 @@ CREATE TABLE `avaliacoes` (
   `panturrilhaDireita` float NOT NULL,
   `panturrilhaEsquerda` float NOT NULL,
   `dataAvaliacao` date NOT NULL,
-  `dataAtualizacao` date NOT NULL
+  `dataAtualizacao` date NOT NULL,
+  `link` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `avaliacoes`
 --
 
-INSERT INTO `avaliacoes` (`id`, `idAluno`, `peso`, `altura`, `imc`, `pescoco`, `ombros`, `peitoral`, `abdomen`, `cintura`, `quadril`, `bicepsDireito`, `bicepsEsquerdo`, `antebracoDireito`, `antebracoEsquerdo`, `coxaDireita`, `coxaEsquerda`, `panturrilhaDireita`, `panturrilhaEsquerda`, `dataAvaliacao`, `dataAtualizacao`) VALUES
-(1, 9, 85.5, 1.74, 28.2, 41.5, 121.5, 101.5, 96, 92.5, 105, 39.5, 39.5, 30.5, 29.5, 60.5, 60, 42.5, 41.5, '2021-11-20', '2021-11-21');
+INSERT INTO `avaliacoes` (`id`, `idAluno`, `peso`, `altura`, `imc`, `pescoco`, `ombros`, `peitoral`, `abdomen`, `cintura`, `quadril`, `bicepsDireito`, `bicepsEsquerdo`, `antebracoDireito`, `antebracoEsquerdo`, `coxaDireita`, `coxaEsquerda`, `panturrilhaDireita`, `panturrilhaEsquerda`, `dataAvaliacao`, `dataAtualizacao`, `link`) VALUES
+(1, 1, 80, 1.75, 26.1, 40, 120, 90, 80, 60, 80, 40, 40, 30, 30, 60, 60, 50, 50, '2021-11-26', '2021-11-28', '$2y$10$hWNbxw8bNzvsXk4kShUvH.H0Vmeg.MWRRs5s20Qo3CSeNJ90WIpBe'),
+(2, 1, 78, 1.78, 24.6, 41, 130, 95, 75, 80, 85, 45, 45, 35, 35, 65, 65, 55, 55, '2021-11-27', '2021-11-28', '$2y$10$EUw7cQau8dSkW.39ZpcpZu30GCyBBvmKXVlJG.QMXndJRAus3C87S'),
+(3, 1, 75, 1.78, 23.7, 45, 140, 100, 70, 72, 80, 50, 50, 40, 40, 80, 80, 70, 70, '2021-11-28', '2021-11-28', '$2y$10$ZEmxFghBiqgcv23wXnLsjew8UTfMmjCAmf9lQiY66/zPBO24dS5hm');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `configs`
+--
+
+CREATE TABLE `configs` (
+  `id` int(11) NOT NULL,
+  `magreza` text NOT NULL,
+  `normal` text NOT NULL,
+  `sobrepeso` text NOT NULL,
+  `obesidade` text NOT NULL,
+  `obesidadeGrave` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `configs`
+--
+
+INSERT INTO `configs` (`id`, `magreza`, `normal`, `sobrepeso`, `obesidade`, `obesidadeGrave`) VALUES
+(1, 'Você está abaixo do peso ideal. Isso pode ser apenas uma característica pessoal, mas também pode ser um sinal de desnutrição ou de algum problema de saúde.', 'Parabéns, você está com o peso normal. Recomendamos que mantenha hábitos saudáveis em seu dia a dia. Especialistas sugerem ingerir de 4 a 5 porções diárias de frutas, verduras e legumes, além da prática de exercícios físicos - pelo menos 150 minutos semanais.', 'Atenção! Alguns quilos a mais já são suficientes para que algumas pessoas desenvolvam doenças associadas, como diabetes e hipertensão. É importante rever seus hábitos.', 'Sinal de alerta! O excesso de peso é fator de risco para desenvolvimento de outros problemas de saúde. A boa notícia é que uma pequena perda de peso já traz benefícios à saúde.', 'Sinal vermelho! Ao atingir este nível de IMC, o risco de doenças associadas é muito alto.');
 
 -- --------------------------------------------------------
 
@@ -149,8 +170,8 @@ CREATE TABLE `grupos` (
 --
 
 INSERT INTO `grupos` (`id`, `nome`, `condicao`) VALUES
-(1, 'bodybuilder', 's'),
-(2, 'crossFit', 's');
+(1, 'crossfit', 'n'),
+(2, 'bodybuilder', 's');
 
 -- --------------------------------------------------------
 
@@ -168,17 +189,6 @@ CREATE TABLE `horarios` (
   `inicio` datetime NOT NULL,
   `fim` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `horarios`
---
-
-INSERT INTO `horarios` (`id`, `idAluno`, `titulo`, `cor`, `exercicio`, `descricao`, `inicio`, `fim`) VALUES
-(12, 6, 'sdasdasd', '#ffc107', 3, 'asdasdasd', '2021-11-02 00:00:00', '2021-11-03 00:00:00'),
-(13, 6, 'agachamento profundo', '#17a2b8', 3, '3 sessões de 20 agachamentos com pesos de 300kg', '2021-11-03 00:00:00', '2021-11-04 00:00:00'),
-(14, 6, 'supino de alta performance', '#dc3545', 1, '10 sessões de 10 repetições  30kg', '2021-11-10 00:00:00', '2021-11-13 00:00:00'),
-(16, 9, 'agachamento', '#17a2b8', 3, 'agachamento profundo', '2021-11-17 00:00:00', '2021-11-18 00:00:00'),
-(17, 9, 'asdasdasdasd', '#dc3545', 3, 'sdasdasdasdasd', '2021-11-18 00:00:00', '2021-11-22 00:00:00');
 
 --
 -- Índices para tabelas despejadas
@@ -201,6 +211,12 @@ ALTER TABLE `alunos`
 -- Índices para tabela `avaliacoes`
 --
 ALTER TABLE `avaliacoes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `configs`
+--
+ALTER TABLE `configs`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -235,12 +251,18 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT de tabela `alunos`
 --
 ALTER TABLE `alunos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `avaliacoes`
 --
 ALTER TABLE `avaliacoes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `configs`
+--
+ALTER TABLE `configs`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
@@ -253,13 +275,13 @@ ALTER TABLE `exercicios`
 -- AUTO_INCREMENT de tabela `grupos`
 --
 ALTER TABLE `grupos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `horarios`
 --
 ALTER TABLE `horarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
