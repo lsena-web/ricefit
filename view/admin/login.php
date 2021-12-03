d9305929c09de9736e73de5dd118caf3b96c80ab
Lucas <80419418+lsena-web@users.noreply.github.com>
2021-12-03 17:13:27 -0300

    final

--- view/admin/login.php
+++ view/admin/login.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="img/short.png">
     <title>riceFit</title>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
